1b3681aa1481a59e962ab77e1241e244eb407f82
jeffgallini <52107906+jeffgallini@users.noreply.github.com>
2021-04-15 09:32:35 -0500

    Updated dashboard to have small data in it

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ import plotly.express as px
 
 # Data Manipulation -----------
 
+## Global Variables
 
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
 color1='darkred'
 color2='orange'
 mytitle='Shot Analysis'
@@ -20,8 +18,37 @@ myheading='Hockey Analytics'
 label1='IBU'
 label2='ABV'
 githublink='https://github.com/dva-sports-analytics/nhl-dashboard'
-sourceurl='https://www.flyingdog.com/beers/'
+sourceurl='http://www.nhl.com/stats/'
 
+
+## Hockey Dataframe
+# I took the data manipulation done by James/Brent to get the data in quickly
+df = pd.read_csv('data/shots.csv')
+df.rename(columns={"result.secondaryType": "shot_type", "team.triCode": "team"}, inplace=True)
+df['scored'] = df['event_type'].apply(lambda event: 1 if event == "GOAL" else 0)
+df['is_rebound_attempt'] = df['time_since_last_shot'].apply(lambda x: True if x <= 5 else False)
+df['shot_type'] = df['shot_type'].apply(lambda x: 'Wrist Shot' if pd.isna(x) else x)
+
+select_df = df[['game_id', 'team', 'scored', 'distance_to_goal', 'shot_type', 'is_rebound_attempt']]
+
+
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[35, 60, 85, 75]
+abv_values=[5.4, 7.1, 9.2, 4.3]
+
+
+#### Shot Summary DataFrame
+
+summarized_shots = select_df.groupby(['shot_type', "is_rebound_attempt"])\
+         .agg({'scored':["sum", "count"], "distance_to_goal":"mean"})\
+         .reset_index()
+
+summarized_shots["Accuracy"] = summarized_shots["scored"]["sum"] / summarized_shots["scored"]["count"]
+
+summarized_shots["Points"] = summarized_shots["scored"]["sum"]
+summarized_shots["Avg_Distance_to_Goal"] = summarized_shots["distance_to_goal"]["mean"]
+
+summarized_shots.drop(["scored", 'distance_to_goal'], axis = 1)
 
 # Modeling --------------------
 
@@ -51,6 +78,15 @@ beer_layout = go.Layout(
 
 beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
+
+shot_type_Bar = px.bar(summarized_shots, x = 'shot_type', y = "Points",
+                       labels = {"shot_type":"Shot Type"},
+                       color = "is_rebound_attempt",
+                       title = "Points by Shot Type", 
+                       hover_data=['Accuracy', 'Avg_Distance_to_Goal'])\
+                  .update_xaxes(categoryorder = "total descending")
+
+
 #Set Up dashboard -------------
 
 
@@ -67,8 +103,8 @@ app.title=tabtitle
 app.layout = html.Div(children=[
     html.H1(myheading),
     dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
+        id='shotType',
+        figure=shot_type_Bar
     ),
     html.A('Code on Github', href=githublink),
     html.Br(),
