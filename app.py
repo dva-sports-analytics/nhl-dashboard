20b19df3d22618da317c923f0accb036992bf1f2
jeffgallini <gallinij@gmail.com>
2021-04-14 19:58:45 -0500

    Updating app to have required packages and doing some limited edits to the page. Changed Flavicon to be Hockey Icon

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import numpy as np
+import pandas as pd
 
 ########### Define your variables
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
